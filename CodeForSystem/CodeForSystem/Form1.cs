64693e76e973fdd0efc24136941109a05b0a9046
ademyldrm <yildirimadem_58@hotmail.com>
2013-11-19 20:38:44 +0200

    CodeForSystem

--- CodeForSystem/CodeForSystem/Form1.cs
+++ CodeForSystem/CodeForSystem/Form1.cs
@@ -3,17 +3,39 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
-namespace CodeForSystem
+using System.Data.SqlClient;
+namespace AnaOkuluBilisim
 {
-    public partial class Form1 : Form
+    public partial class ChangePassword : Form
     {
-        public Form1()
+        public ChangePassword()
         {
             InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection("Data Source=.; database=AnaOkuluDB;integrated security=true");
+        SqlCommand cmd;
+        string qry;
+
+        private void btncreatepwd_Click_1(object sender, EventArgs e)
+        {
+            Information.Text = "";
+            try
+            {
+                if (validatedata() == 0)
+                {
+                    errorProvider1.Clear();
+                    if (txtnewpwd.Text.Length == txtconfirmpwd.Text.Length)
+                    {
+                        qry = "update pwd  set createpassword='" + txtnewpwd.Text + "' where userid='" + txtuserid.Text + "'and createpassword='" + txtoldpwd.Text + "'";
+                        cmd = new SqlCommand(qry, conn);
+                        //MessageBox.Show(qry);
+                        conn.Open();
+                        int row = cmd.ExecuteNonQuery();
+                        if (row > 0)
+                       
+            return i;
         }
     }
 }
