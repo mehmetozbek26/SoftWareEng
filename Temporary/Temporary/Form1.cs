59ea823797d492078f87e42056e27e7984b17901
ademyldrm <yildirimadem_58@hotmail.com>
2013-11-20 20:54:46 +0200

    Temp

--- Temporary/Temporary/Form1.cs
+++ Temporary/Temporary/Form1.cs
@@ -15,7 +15,24 @@ namespace Temporary
         {
             InitializeComponent();
         }
+        public static PropertyInfo GetProperty<T>(Expression<Func<T, Object>> propertyExpression)
+        {
+            var lambda = propertyExpression as LambdaExpression;
+            MemberExpression memberExpression;
+            if (lambda.Body is UnaryExpression)
+            {
+                var unaryExpression = lambda.Body as UnaryExpression;
+                memberExpression = unaryExpression.Operand as MemberExpression;
+            }
+            else
+            {
+                memberExpression = lambda.Body as MemberExpression;
+            }
 
+            var propertyInfo = memberExpression.Member as PropertyInfo;
+
+            return propertyInfo;
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
