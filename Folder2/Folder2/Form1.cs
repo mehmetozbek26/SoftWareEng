a1c7cf22c8feb00733a54afc2cb58deafdee5b58
ademyldrm <yildirimadem_58@hotmail.com>
2013-11-21 21:06:12 +0200

    Folder2

---
+++ Folder2/Folder2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Folder2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            public sealed class MixingDeskBuilder
+{
+    private const int DEFAULT_CHANNELS = 10;
+    private int channels = DEFAULT_CHANNELS;
+
+    public int Channels
+    {
+        get { return channels; }
+        set { channels = value; }
+    }
+
+    public MixingDesk GetValue()
+    {
+        return new MixingDesk(channels);
+    }
+}
+        }
+    }
+}
