66eaf39b9c858e3f99748de157d017fc0e467eda
ademyldrm <yildirimadem_58@hotmail.com>
2013-11-23 21:18:19 +0200

    Folder4

--- Folder2/Folder2/Form1.cs
+++ Folder2/Folder2/Form1.cs
@@ -65,5 +65,44 @@ internal sealed class PropertyData<T> : IPropertyData<T>, IInterceptableProperty
         this.returnValue = returnValue;
         return this;
     }
+    internal sealed class PropertyData<T> : IPropertyData<T>, IInterceptablePropertyData, ISupportExceptions
+{
+    private Exception exceptionToThrow;
+
+    public PropertyData()
+    {
+        eventsToRaise = new List<EventWrapper>();
+        setHasBeenCalled = 0;
+        getHasBeenCalled = 0;
+        exceptionToThrow = null;
+    }
+
+    public IPropertyData<T> ThrowsOnSet<TEx>() where TEx : Exception
+    {
+        exceptionToThrow = (Exception)Activator.CreateInstance<TEx>();
+        return this;
+    }
+
+    public IPropertyData<T> ThrowsOnSet(Exception ex)
+    {
+        exceptionToThrow = ex;
+        return this;
+    }
+
+    #region ISupportExceptions members
+
+    Exception ISupportExceptions.ExceptionToThrow
+    {
+        get { return exceptionToThrow; }
+        set { exceptionToThrow = value; }
+    }
+
+    bool ISupportExceptions.HasException
+    {
+        get { return exceptionToThrow != null; }
+    }
+
+    #endregion
+}
 }
 }
