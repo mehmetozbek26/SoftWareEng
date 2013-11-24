960c992400db9c9f54d8de6f3c915a9e16ee948c
ademyldrm <yildirimadem_58@hotmail.com>
2013-11-24 21:21:52 +0200

    Folder4Del

--- Folder2/Folder2/Form1.cs
+++ Folder2/Folder2/Form1.cs
@@ -8,64 +8,7 @@ using System.Text;
 using System.Windows.Forms;
 
 namespace Folder2
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            public sealed class MixingDeskBuilder
-{
-    private const int DEFAULT_CHANNELS = 10;
-    private int channels = DEFAULT_CHANNELS;
-
-    public int Channels
-    {
-        get { return channels; }
-        set { channels = value; }
-    }
-
-    public MixingDesk GetValue()
-    {
-        return new MixingDesk(channels);
-    }
-}public class SamplerBuilder
-{
-    private StudioBuilder parent;
-    private int hardDiskSize;
-    private string model;
-
-    public SamplerBuilder(StudioBuilder parent)
-    {
-        this.parent = parent;
-    }
-
-    public SamplerBuilder DiskSize(int hardDiskSize)
-    {
-        this.hardDiskSize = hardDiskSize;
-        return this;
-    }
-
-    public SamplerBuilder Model(string model)
-    {
-        this.model = model;
-        return this;
-    }
-
 internal sealed class PropertyData<T> : IPropertyData<T>, IInterceptablePropertyData, ISupportExceptions
-{
-    private object returnValue;
-
-    public IPropertyData<T> Returns(object returnValue)
-    {
-        this.returnValue = returnValue;
-        return this;
-    }
-    internal sealed class PropertyData<T> : IPropertyData<T>, IInterceptablePropertyData, ISupportExceptions
 {
     private Exception exceptionToThrow;
 
@@ -103,6 +46,4 @@ internal sealed class PropertyData<T> : IPropertyData<T>, IInterceptableProperty
     }
 
     #endregion
-}
-}
 }
