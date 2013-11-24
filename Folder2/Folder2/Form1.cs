9b76f8363d1bd1964ec307c26b909cd22eac7a76
ademyldrm <yildirimadem_58@hotmail.com>
2013-11-24 21:29:35 +0200

    Category

--- Folder2/Folder2/Form1.cs
+++ Folder2/Folder2/Form1.cs
@@ -8,42 +8,157 @@ using System.Text;
 using System.Windows.Forms;
 
 namespace Folder2
-internal sealed class PropertyData<T> : IPropertyData<T>, IInterceptablePropertyData, ISupportExceptions
+innamespace faktoriyel_hesaplam
 {
-    private Exception exceptionToThrow;
+public partial class Form1 : Form
+{
+public Form1()
+{
+InitializeComponent();
+}
+public static class islem
+{
+public static void azalt(ref int s1)
+{
+s1--;
+}
+public static void arttir(ref int s1)
+{
+s1++;
+}
+public static int faktoriyel(int sayi)
+{
+int sonuc = 1;
+for (int i = 1; i <= sayi; i++)
+{
+sonuc *= i;
 
-    public PropertyData()
-    {
-        eventsToRaise = new List<EventWrapper>();
-        setHasBeenCalled = 0;
-        getHasBeenCalled = 0;
-        exceptionToThrow = null;
-    }
+}
+return sonuc;
+}
+}
+int sonuc;
+private void button1_Click(object sender, EventArgs e)
+{
+int sayi = Convert.ToInt32(textBox1.Text);
+sonuc = islem.faktoriyel(sayi);
+label1.Text = "sonuc:" + sonuc.ToString();
 
-    public IPropertyData<T> ThrowsOnSet<TEx>() where TEx : Exception
-    {
-        exceptionToThrow = (Exception)Activator.CreateInstance<TEx>();
-        return this;
-    }
+}
 
-    public IPropertyData<T> ThrowsOnSet(Exception ex)
-    {
-        exceptionToThrow = ex;
-        return this;
-    }
+private void button3_Click(object sender, EventArgs e)
+{
+int sayi = Convert.ToInt32(textBox1.Text);
+islem.azalt(ref sayi);
+textBox1.Text = sayi.ToString();
+sonuc = islem.faktoriyel(sayi);
+label1.Text = sonuc.ToString();
+}
 
-    #region ISupportExceptions members
+private void button2_Click(object sender, EventArgs e)
+{
+int sayi = Convert.ToInt32(textBox1.Text);
+islem.arttir(ref sayi);
+label1.Text = sonuc.ToString();
+textBox1.Text = sayi.ToString();
+}
+}
+}
+28 Kasım 2013 21:34
+ Adsız dedi ki...
+namespace WindowsFormsApplication1
+{
+public partial class Form1 : Form
+{
+public Form1()
+{
+InitializeComponent();
+}
 
-    Exception ISupportExceptions.ExceptionToThrow
-    {
-        get { return exceptionToThrow; }
-        set { exceptionToThrow = value; }
-    }
+public static int []atoz(int []w)
+{
+for(int i=1; iw[i-1])
+{
+int tasiyiciSayi=w[j];
+w[i]=w[i-1];
+w[i-1]=tasiyiciSayi;
+}
+}
 
-    bool ISupportExceptions.HasException
-    {
-        get { return exceptionToThrow != null; }
-    }
 
-    #endregion
+return w;
+}
+public static int[] ztoo(int[] d)
+{
+for (int i = 1; i < d.Length; i++)
+{
+for (int j = i; j >= 1; j--)
+{
+if (d[j] < d[j - 1])
+{
+int tasiyiciSayi = d[j];
+d[j] = d[j - 1];
+d[j - 1] = tasiyiciSayi;
+}
+}
+
+}
+return d;
+}
+int[] dizi = new int[10];
+byte i;
+int d;
+
+private void button1_Click(object sender, EventArgs e)
+{
+int sayac = 0;
+try
+{
+for (i = 0; i < 10; i++)
+{
+sayac++;
+dizi[i] = int.Parse(Microsoft.VisualBasic.Interaction.InputBox(sayac + "sayıyı girin", "sayi girişi", "", 100, 100));
+if (dizi[i] == 0) break;
+}
+}
+catch (FormatException)
+{
+MessageBox.Show("hatalı giriş");
+i--;
+}
+}
+
+private void button2_Click(object sender, EventArgs e)
+{
+listBox1.Items.Clear();
+foreach (int nesne in dizi)
+if (nesne != 0)
+listBox1.Items.Add(nesne.ToString());
+}
+
+private void button3_Click(object sender, EventArgs e)
+{
+
+listBox1.Items.Clear();
+dizi = sıralama.ztoo(dizi);
+for (int i = 0; i < dizi.Length; i++)
+{
+if (dizi[i] != 0)
+listBox1.Items.Add(dizi[i]);
+}
+
+}
+
+private void button4_Click(object sender, EventArgs e)
+{
+listBox1.Items.Clear();
+dizi=sıralama.atoz(dizi);
+for(int i=0; i<dizi.Length;i++)
+{
+if(dizi[i]!=0)
+ListBox1.Items.Add(dizi[i]);
+}
+
+}
+}
 }
